5aeef9ab4f06523e5d3e7660835e31f87ff3825e
Roaa Khalil <rooaakhalil123@gmail.com>
2022-10-03 06:42:00 -0400

    add soloution

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,16 @@
 """Frequencies function."""
-"""ENTER YOUR SOLUTION HERE!"""
+
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    stringItems = [str(x) for x in items]
+
+    for s in stringItems:
+
+        if s in frequencies:
+            frequencies[i] += 1
+        else:
+
+            frequencies[i] = 1
+        
     return frequencies
